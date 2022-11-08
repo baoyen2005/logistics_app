b062831332ca3a30ed8a1a6d20328923394eb11e
yenlb <yenlb@vnpay.com>
2022-11-08 17:39:16 +0700

    bug-add-new-order

---
+++ app/src/main/java/com/example/bettinalogistics/ui/activity/google_map/InfoWindowAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.bettinalogistics.ui.activity.google_map
+
+import android.content.Context
+import android.location.Location
+import android.view.View
+import com.example.bettinalogistics.databinding.InfoWindowLayoutBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+
+//class InfoWindowAdapter(private val location:Location, context: Context) : GoogleMap.InfoWindowAdapter{
+//    private val binding : InfoWindowLayoutBinding?= null
+//    override fun getInfoContents(p0: Marker): View? {
+//
+//    }
+//
+//    override fun getInfoWindow(p0: Marker): View? {
+//    }
+//}
